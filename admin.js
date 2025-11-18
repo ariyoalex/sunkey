6c9aa90a1671b769f87f2ddd6af676bdc35ef01d
Alex Ariyo <lxndrbrain@gmail.com>
2025-11-18 23:08:11 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = 'https://v2.hireboothub.com/spin-win-api/api.php';
+const API_URL = 'https://v2.hireboothub.com/spin-win-api/api';
 
 // Rate Limiting Configuration
 const RATE_LIMIT = {
